704f3efd58f8c486d22a78e2601c63d03fc319a2
Myles Nottingham <MylesNottingham@gmail.com>
2023-02-23 13:39:53 -0700

    Add place array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = ["Tom", "David", "Michael", "John", "Sarah"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+places = ["Mexico", "Canada", "Spain", "Paris", "London"]
+places.shift
+puts places
